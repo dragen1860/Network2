fff1b5a387346b57c49f40aa2c85d1676d05ed03
dragen1860 <dragen@live.cn>
2018-01-23 15:34:26 +0800

    reduce network complexity, converge faster

--- matrix.py
+++ matrix.py
@@ -19,7 +19,9 @@ class Matrix(nn.Module):
 
 		# (256, 4, 4)
 		self.repnet = nn.Sequential(repnet_deep(False), # (1024, 14, 14)
-		                            nn.MaxPool2d(3),
+		                            nn.MaxPool2d(5,3),
+		                            # nn.Conv2d(1024, 256, kernel_size=5, stride=3),
+		                            nn.BatchNorm2d(1024),
 		                            nn.ReLU(inplace=True))
 		# we need to know the feature dim, so here is a forwarding.
 		repnet_sz = self.repnet(Variable(torch.rand(2, 3, 224, 224))).size()
