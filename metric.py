68efcd00e4f3dbda9d2f0582df681329bd720525
dragen1860 <dragen@live.cn>
2018-02-01 15:06:42 +0800

    metric is working

--- metric.py
+++ metric.py
@@ -69,16 +69,14 @@ class Metric(nn.Module):
 		                       nn.ReLU(inplace=True))
 
 		# output distance between two pairs.
-		self.f = nn.Sequential(nn.BatchNorm1d(256),
-								nn.Linear(256, 256),
+		self.f = nn.Sequential(nn.Linear(256, 256),
 		                       nn.ReLU(inplace=True),
 		                       nn.Linear(256, 256),
 		                       nn.Dropout(),
 		                       nn.ReLU(inplace=True),
-		                       nn.Linear(256, 64),
+		                       nn.Linear(256, 128),
 		                       nn.ReLU(inplace=True),
-		                       nn.Linear(64, 1),
-		                       nn.Sigmoid())
+		                       nn.Linear(128, 64))
 
 
 		coord = np.array([(i / self.d , j / self.d) for i in range(self.d) for j in range(self.d)])
@@ -86,6 +84,17 @@ class Metric(nn.Module):
 		self.coord = self.coord.unsqueeze(0).unsqueeze(0)
 		# print('self.coord:', self.coord.size(),self.coord) # [batchsz:1, setsz:1, 2, self.d, self.d]
 
+
+	def squash(self, input_tensor):
+		"""Applies norm nonlinearity (squash) to a capsule layer.
+		input: [b, querysz, setsz]
+		Returns: [b, querysz, setsz]
+		  A tensor with same shape as input (rank 3) for output of this layer.
+		"""
+		# input: [b, querysz, setsz] norm: [b, querysz, 1]
+		norm = torch.norm(input_tensor, p=2, dim=2, keepdim=True)
+		norm_squared = norm * norm
+		return (input_tensor / norm) * (norm_squared / (1 + norm_squared))
 
 
 	def forward(self, support_x, support_y, query_x, query_y, train = True):
@@ -107,13 +116,13 @@ class Metric(nn.Module):
 		# [b, querysz, c_, h, w] => [b*querysz, c_, h, w] => [b*querysz, c, d, d] => [b, querysz, c, d, d]
 		query_xf = self.repnet(query_x.view(batchsz * querysz, c_, h, w)).view(batchsz, querysz, c, d, d)
 
-		# # sum over k_shot imgs' features to ensemble
-		# # [b, setsz, c, d, d] => [b, n_way, k_shot, c, d, d] => [b, n_way, c, d, d], sum over k_shot dim
-		# support_xf = support_xf.view(batchsz, self.n_way, self.k_shot, c, d, d).sum(2)
-		# # update setsz now
-		# setsz = self.n_way
-		# # [b, n_way*k_shot] => [b, n_way]
-		# support_y = support_y[:, ::self.k_shot]
+		# sum over k_shot imgs' features to ensemble
+		# [b, setsz, c, d, d] => [b, n_way, k_shot, c, d, d] => [b, n_way, c, d, d], sum over k_shot dim
+		support_xf = support_xf.view(batchsz, self.n_way, self.k_shot, c, d, d).sum(2)
+		# update setsz now
+		setsz = self.n_way
+		# [b, n_way*k_shot] => [b, n_way]
+		support_y = support_y[:, ::self.k_shot]
 
 
 		## now make the combination between two pairs
@@ -140,8 +149,10 @@ class Metric(nn.Module):
 		# [b*querysz*setsz*d^4, -1] => [b*querysz*setsz*d^4, -1] => [b*querysz*setsz, d^4, -1] => [b*querysz*setsz, -1]
 		x_f = x_f.view(batchsz * querysz * setsz, d*d * d*d, -1).sum(1) # the last dim can be derived by layer setting
 		# push to F network
-		# [batchsz * querysz * setsz, -1] => [batchsz * querysz * setsz, 1] => [b, querysz, setsz]
-		dist = 1 - self.f(x_f).view(batchsz, querysz, setsz)
+		# [batchsz * querysz * setsz, -1] => [batchsz * querysz * setsz, vector] => [b, querysz, setsz]
+		score = self.f(x_f).view(batchsz, querysz, setsz, -1).norm(2, dim=3)
+		# squash to [0,1]
+		score = self.squash(score)
 
 		# build its label
 		# [b, setsz] => [b, 1, setsz] => [b, querysz, setsz]
@@ -150,9 +161,9 @@ class Metric(nn.Module):
 		query_yf = query_y.unsqueeze(2).expand(batchsz, querysz, setsz)
 		# eq: [b, querysz, setsz] => [b, querysz, setsz] and convert byte tensor to float tensor
 		# this is the label of distance, where 0 means positive pairs
-		label = torch.ne(support_yf, query_yf).float()
+		label_an = torch.ne(support_yf, query_yf)
 		# similarity label, for the following select dist(a,p) usage.
-		label_sim = torch.eq(support_yf, query_yf)
+		label_ap = torch.eq(support_yf, query_yf)
 
 
 
@@ -162,7 +173,7 @@ class Metric(nn.Module):
 		# label: [b, querysz, setsz]
 		if train:
 			# construct triplet loss.
-			# loss = sum{ max(dist(a,p) - min_neg(dist(a,n)) + margin, 0) }
+			# loss = sum{ sum[max(dist(a,p) - (dist(a,n)) + margin, 0)] over all setsz-1 negative pairs }
 			margin = 0.2
 			# min_n(dist(a,n))
 			# [b, querysz, setsz] * [b, querysz, setsz]
@@ -171,38 +182,31 @@ class Metric(nn.Module):
 			# dist:         [0.4, 0.3, 0.2, 0.8, 0.9] * [1, 1, 0, 1, 1] => [0.4, 0.3, 0, 0.8, 0.9]
 			# select the 2nd small element, since it's min_n(dist(a,n))
 
-			# # [b, querysz, setsz] => [b, querysz]
-			# min_neg, _ = torch.kthvalue(torch.mul(label, dist).cpu(), 2)
-			# min_neg = min_neg.cuda()
+			# dist: [b, querysz, setsz], mask: [b, querysz, setsz], only one on setsz dim
+			# => dist_ap: [b, querysz] => [b, querysz, setsz-1]
+			score_ap = torch.masked_select(score, label_ap).view(batchsz, querysz, 1).expand(batchsz, querysz, setsz - 1)
+			# dist: [b, querysz, setsz], mask: [b, querysz, setsz], (n-way - 1) on setsz dim
+			# => dist_ap: [b, querysz, setsz - 1]
+			score_an = torch.masked_select(score, label_an).view(batchsz, querysz, setsz - 1)
 
-			# sort [b, querysz, setsz] => [b, querysz, setsz]
-			min_neg, _ = torch.sort(torch.mul(label, dist), dim=2)
-			# [b, querysz, setsz] => [b, querysz], get the 2nd small element
-			min_neg = min_neg[:, :, 1]
 
-			# now select dist(a,p)
-			# torch.where function exists only in torch v0.4, shit!
-			# [b, querysz, setsz] => [N, 3], the last column is positive index
-			indices = label_sim.nonzero()
-			# [N, 3] => [N, 1] => [b, querysz, 1]
-			indices = (indices[...,-1]).contiguous().view(batchsz, querysz, 1)
-			# get from [b, querysz, setsz] with index: positive indices: [b, querysz, 1]
-			# => [b, querysz, 1] => [b, querysz]
-			dist_ap = torch.gather(dist, dim=2, index=indices).view(batchsz, querysz)
 			# [b, querysz] - [b, querysz] + margin
 			# we use relu function to replace max(x, 0) operation
 			# => [b, querysz] => scalar
-			loss = F.relu(dist_ap - min_neg + margin).sum() / batchsz
-			if np.random.randint(100)<3:
-				print('y:', support_y[0].cpu().data.numpy(), query_y[0].cpu().data.numpy())
-				print('ap vs an:', dist_ap[0].cpu().data.numpy(), min_neg[0].cpu().data.numpy())
-				print('dist:', dist[0].cpu().data.numpy())
+			# dist_ap - dist_an + margin => [b, querysz, setsz - 1]
+			# it force score_an < score_ap with a small margin, otherwise will create a loss
+			loss = F.relu(score_an - score_ap + margin).sum()
+
+			if np.random.randint(100) < 1:
+				# [b, querysz, setsz-1]
+				tmp = torch.cat([(score_ap[..., 0]).unsqueeze(2), score_an], dim=2)
+				print(tmp[0].cpu().data.numpy())
 			return loss
 
 		else:
-			# select the closet element
+			# select the highest score element
 			# [b, querysz, setsz] => [b, querysz]
-			_, indices = dist.min(dim=2)
+			_, indices = score.max(dim=2)
 			# support_y: [b, querysz]
 			# indices: [b, querysz]
 			# pred: [b, querysz], global true label
--- metric_train.py
+++ metric_train.py
@@ -4,94 +4,157 @@ from torch import optim
 from torch.autograd import Variable
 from MiniImagenet import MiniImagenet
 from metric import Metric
-from utils import make_imgs
-
-if __name__ == '__main__':
-	from MiniImagenet import MiniImagenet
-	from torch.utils.data import DataLoader
-	from torchvision.utils import make_grid
-	from torch.optim import lr_scheduler
-	from tensorboardX import SummaryWriter
-	from datetime import datetime
-	import random
-	import argparse
+import scipy.stats
+from torch.utils.data import DataLoader
+from torch.optim import lr_scheduler
+import random, sys
+import argparse
+from torch import  nn
 
 
+
+def mean_confidence_interval(accs, confidence = 0.95):
+    n = accs.shape[0]
+    m, se = np.mean(accs), scipy.stats.sem(accs)
+    h = se * scipy.stats.t._ppf( (1 + confidence) / 2, n - 1)
+    return m, h
+
+
+# save best acc info, to save the best model to ckpt.
+best_accuracy = 0
+def evaluation(net, batchsz, n_way, k_shot, imgsz, episodesz, threhold, mdl_file):
+	"""
+	obey the expriment setting of MAML and Learning2Compare, we randomly sample 600 episodes and 15 query images per query
+	set.
+	:param net:
+	:param batchsz:
+	:return:
+	"""
+	k_query = 15
+	mini_val = MiniImagenet('../mini-imagenet/', mode='test', n_way=n_way, k_shot=k_shot, k_query=k_query,
+	                        batchsz=600, resize=imgsz)
+	db_val = DataLoader(mini_val, batchsz, shuffle=True, num_workers=2, pin_memory=True)
+
+	accs = []
+	episode_num = 0 # record tested num of episodes
+
+	for batch_test in db_val:
+		# [60, setsz, c_, h, w]
+		# setsz = (5 + 15) * 5
+		support_x = Variable(batch_test[0]).cuda()
+		support_y = Variable(batch_test[1]).cuda()
+		query_x = Variable(batch_test[2]).cuda()
+		query_y = Variable(batch_test[3]).cuda()
+
+		# we will split query set into 15 splits.
+		# query_x : [batch, 15*way, c_, h, w]
+		# query_x_b : tuple, 15 * [b, way, c_, h, w]
+		query_x_b = torch.chunk(query_x, k_query, dim= 1)
+		# query_y : [batch, 15*way]
+		# query_y_b: 15* [b, way]
+		query_y_b = torch.chunk(query_y, k_query, dim= 1)
+		preds = []
+		net.eval()
+		# we don't need the total acc on 600 episodes, but we need the acc per sets of 15*nway setsz.
+		total_correct = 0
+		total_num = 0
+		for query_x_mini, query_y_mini in zip(query_x_b, query_y_b):
+			# print('query_x_mini', query_x_mini.size(), 'query_y_mini', query_y_mini.size())
+			pred, correct = net(support_x, support_y, query_x_mini.contiguous(), query_y_mini, False)
+			correct = correct.sum() # multi-gpu
+			# pred: [b, nway]
+			preds.append(pred)
+			total_correct += correct.data[0]
+			total_num += query_y_mini.size(0) * query_y_mini.size(1)
+		# # 15 * [b, nway] => [b, 15*nway]
+		# preds = torch.cat(preds, dim= 1)
+		acc = total_correct / total_num
+		print('%.5f,'%acc, end=' ')
+		sys.stdout.flush()
+		accs.append(acc)
+
+		# update tested episode number
+		episode_num += query_y.size(0)
+		if episode_num > episodesz:
+			# test current tested episodes acc.
+			acc = np.array(accs).mean()
+			if acc >= threhold:
+				# if current acc is very high, we conduct all 600 episodes testing.
+				continue
+			else:
+				# current acc is low, just conduct `episodesz` num of episodes.
+				break
+
+
+	# compute the distribution of 600/episodesz episodes acc.
+	global best_accuracy
+	accs = np.array(accs)
+	accuracy, sem = mean_confidence_interval(accs)
+	print('\naccuracy:', accuracy, 'sem:', sem)
+	print('<<<<<<<<< accuracy:', accuracy, 'best accuracy:', best_accuracy, '>>>>>>>>')
+
+	if accuracy > best_accuracy:
+		best_accuracy = accuracy
+		torch.save(net.state_dict(), mdl_file)
+		print('Saved to checkpoint:', mdl_file)
+
+	return accuracy, sem
+
+
+def main():
 	argparser = argparse.ArgumentParser()
-	argparser.add_argument('-n', help='n way', default=3)
-	argparser.add_argument('-k', help='k shot', default=1)
-	argparser.add_argument('-b', help='batch size', default=1)
+	argparser.add_argument('-n', help='n way')
+	argparser.add_argument('-k', help='k shot')
+	argparser.add_argument('-b', help='batch size')
 	argparser.add_argument('-l', help='learning rate', default=1e-3)
 	args = argparser.parse_args()
 	n_way = int(args.n)
 	k_shot = int(args.k)
 	batchsz = int(args.b)
 	lr = float(args.l)
+
 	k_query = 1
 	imgsz = 224
+	threhold = 0.7 if k_shot==5 else 0.59 # threshold for when to test full version of episode
+	mdl_file = 'ckpt/metric%d%d.mdl'%(n_way, k_shot)
+	print('mini-imagnet: %d-way %d-shot lr:%f, threshold:%f' % (n_way, k_shot, lr, threhold))
 
 	# torch.manual_seed(66)
 	# np.random.seed(66)
 	# random.seed(66)
 
-	net = Metric(n_way, k_shot, imgsz).cuda()
+
+	net = nn.DataParallel(Metric(n_way, k_shot, imgsz), device_ids=[0]).cuda()
 	print(net)
-	mdl_file = 'ckpt/metric%d%d.mdl'%(n_way, k_shot)
-	print('mini-imagnet: %d-way %d-shot lr:%f' % (n_way, k_shot, lr))
 
 	if os.path.exists(mdl_file):
-		print('load checkpoint ...', mdl_file)
-		net.load_state_dict(torch.load(mdl_file), strict=False)
+		print('load from checkpoint ...', mdl_file)
+		net.load_state_dict(torch.load(mdl_file))
 	else:
 		print('training from scratch.')
 
+	# whole parameters number
 	model_parameters = filter(lambda p: p.requires_grad, net.parameters())
 	params = sum([np.prod(p.size()) for p in model_parameters])
 	print('Total params:', params)
 
+	# build optimizer and lr scheduler
 	optimizer = optim.Adam(net.parameters(), lr=lr)
-	scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'max', factor=0.5, patience=10, verbose=True)
+	# optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9, nesterov=True)
+	scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'max', factor=0.5, patience=20, verbose=True)
 
-	best_accuracy = 0
 	for epoch in range(1000):
 		mini = MiniImagenet('../mini-imagenet/', mode='train', n_way=n_way, k_shot=k_shot, k_query=k_query,
 		                    batchsz=10000, resize=imgsz)
 		db = DataLoader(mini, batchsz, shuffle=True, num_workers=8, pin_memory=True)
-		mini_val = MiniImagenet('../mini-imagenet/', mode='test', n_way=n_way, k_shot=k_shot, k_query=k_query,
-		                        batchsz=200, resize=imgsz)
-		db_val = DataLoader(mini_val, batchsz, shuffle=True, num_workers=4, pin_memory=True)
 		total_train_loss = 0
 
 		for step, batch in enumerate(db):
-
 			# 1. test
-			total_val_loss = 0
 			if step % 300 == 0:
-				total_correct = 0
-				total_num = 0
-
-				for j, batch_test in enumerate(db_val):
-					support_x = Variable(batch_test[0]).cuda()
-					support_y = Variable(batch_test[1]).cuda()
-					query_x = Variable(batch_test[2]).cuda()
-					query_y = Variable(batch_test[3]).cuda()
-
-					net.eval()
-					pred, correct = net(support_x, support_y, query_x, query_y, False)
-					total_correct += correct.data[0]
-					total_num += query_y.size(0) * query_y.size(1)
-
-
-				accuracy = total_correct / total_num
-				if accuracy > best_accuracy :
-					best_accuracy = accuracy
-					torch.save(net.state_dict(), mdl_file)
-					torch.save(net, mdl_file[:-4]+'.whl')
-					print('Saved to checkpoint and whole mdl! ', mdl_file, mdl_file[:-4]+'.whl')
-
-				print('<<<<>>>>accuracy:', accuracy, 'best accuracy:', best_accuracy)
-
-				# scheduler.step(accuracy)
+				# evaluation(net, batchsz, n_way, k_shot, imgsz, episodesz, threhold, mdl_file):
+				accuracy, sem = evaluation(net, batchsz, n_way, k_shot, imgsz, 100, threhold, mdl_file)
+				scheduler.step(accuracy)
 
 			# 2. train
 			support_x = Variable(batch[0]).cuda()
@@ -101,6 +164,7 @@ if __name__ == '__main__':
 
 			net.train()
 			loss = net(support_x, support_y, query_x, query_y)
+			loss = loss.sum() / support_x.size(0) # multi-gpu, divide by total batchsz
 			total_train_loss += loss.data[0]
 
 			optimizer.zero_grad()
@@ -112,3 +176,8 @@ if __name__ == '__main__':
 				print('%d-way %d-shot %d batch> epoch:%d step:%d, loss:%f' % (
 				n_way, k_shot, batchsz, epoch, step, total_train_loss) )
 				total_train_loss = 0
+
+
+
+if __name__ == '__main__':
+	main()
